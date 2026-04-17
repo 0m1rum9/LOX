c66ad84d4451ef9a7199e36660f3baf190c36764
0m1rum9 <almseylkhan@gmail.com>
2026-04-17 20:19:58 +0500

    added logout functionality

--- backend/users_auth/urls.py
+++ backend/users_auth/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
+    TokenBlacklistView,
     TokenObtainPairView,
     TokenRefreshView,
 )
@@ -8,6 +9,7 @@ from users_auth.views import register
 
 urlpatterns = [
     path("register/", view=register, name="register"),
-    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("logout/", TokenBlacklistView.as_view(), name="logout"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
