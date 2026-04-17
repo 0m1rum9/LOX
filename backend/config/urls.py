77e12b43b936db8cbbe00d9a23b8479e75209f5f
0m1rum9 <almseylkhan@gmail.com>
2026-04-17 20:12:16 +0500

    fixed naming problem -> renamed project auth to users_auth

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from users.urls import urlpatterns as users_urls
-from auth.urls import urlpatterns as auth_urls
+from users_auth.urls import urlpatterns as auth_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- backend/users_auth/urls.py
+++ backend/users_auth/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from auth.views import register
+from users_auth.views import register
 
 
 urlpatterns = [
