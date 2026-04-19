901b4ff3707b288805f99c9ef9f9e65f93a6c75b
pxn4 <pxn46@vk.com>
2026-04-19 21:37:53 +0500

    added search/row icon page + login-register

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
-  templateUrl: './register.html',
-  styleUrl: './register.css',
+  imports: [FormsModule, NgIf, RouterLink],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  username = ''
+  email = ''
+  password = ''
+  error = ''
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  onRegister() {
+    if (!this.username || !this.email || !this.password) {
+      this.error = 'Заполните все поля'
+      return
+    }
+    this.auth.register(this.username, this.email, this.password).subscribe({
+      next: () => this.router.navigate(['/login']),
+      error: () => this.error = 'Ошибка регистрации. Попробуйте другой email'
+    })
+  }
 }
