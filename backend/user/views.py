e11b8b8f7c19d62213ff6617492b4279d257d30b
0m1rum9 <almseylkhan@gmail.com>
2026-04-19 19:35:26 +0500

    fix: fixed UserNotFound error for it to response with 404

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 
 from http import HTTPMethod, HTTPStatus
@@ -13,4 +14,6 @@ from user.serializers import UserSerializer
 @api_view(http_method_names=[HTTPMethod.GET])
 @permission_classes([IsAuthenticated])
 def get_user(request: Request, id: int) -> Response:
-    return Response(UserSerializer(User.objects.get(id=id)).data, status=HTTPStatus.OK)
+    return Response(
+        UserSerializer(get_object_or_404(User, id=id)).data, status=HTTPStatus.OK
+    )
